f1c38c08d96677c68e7db495a04ea8b8ee52ad7f
locoioioi <118866346+locoioioi@users.noreply.github.com>
2023-07-21 23:14:19 +0700

    a

---
+++ assest/js/script.js
@@ -0,0 +1,20 @@
+var mobileBar = document.querySelector(".bar-container .search-bar");
+var searchBar = document.querySelector(".bar-container");
+var currentHeight = searchBar.clientHeight;
+var menuItem = document.querySelectorAll(".bar-container li a");
+
+// Đóng mở mobile menu
+mobileBar.onclick = function() {
+    var isClosed = searchBar.offsetHeight === currentHeight;
+    if (isClosed) { 
+        searchBar.style.height = "auto";
+    } else {
+        searchBar.style.height = null;
+    }
+}
+// tự động đóng menu 
+for (var i = 0; i < menuItem.length; i++) { 
+    menuItem[i].onclick = function() { 
+        searchBar.style.height = null;
+    }
+}
